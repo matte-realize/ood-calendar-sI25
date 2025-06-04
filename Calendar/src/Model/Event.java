a3e9759575fdb33e71aa5997ebf0751311b2e725
paulwisnaskas <paul.wisnaskas@gmail.com>
2025-06-04 09:58:29 -0400

    added event class

--- Calendar/src/Model/Event.java
+++ Calendar/src/Model/Event.java
@@ -11,6 +11,23 @@ public class Event implements EventInterface {
   private Location location;
   private Status status;
 
-  public Event() throws IllegalArgumentException {
+  public Event(String subject, LocalDateTime startDateTime) throws IllegalArgumentException {
+    this.subject = subject;
+    this.startDateTime = startDateTime;
+  }
+
+  public static class CustomEventBuilder extends EventBuilder<CustomEventBuilder> {
+    public EventInterface build() {
+      Event event = new Event(this.subject, this.startDateTime);
+      event.description = this.description;
+      event.location = this.location;
+      event.status = this.status;
+      event.endDateTime = this.endDateTime;
+      return event;
+    }
+
+    protected CustomEventBuilder returnBuilder() {
+      return this;
+    }
   }
 }
--- Calendar/src/Model/EventBuilder.java
+++ Calendar/src/Model/EventBuilder.java
@@ -1,4 +1,53 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package Model;
 
-public class EventBuilder {
+import java.time.LocalDateTime;
+
+public abstract class EventBuilder<T extends EventBuilder<T>> {
+  protected String subject;
+  protected LocalDateTime startDateTime
+  protected LocalTime startTime;
+  protected LocalTime endTime;
+  protected String description;
+  protected Location location;
+  protected Status status;
+
+  public T setEndDateTime(LocalDateTIme endDateTime) {
+    this.endDateTime = endDateTime;
+    return (T)this.returnBuilder();
+  }
+
+  public T setStartDateTime(LocalDateTime startDateTime) {
+    this.startDateTime = startDateTime;
+    return (T)this.returnBuilder();
+  }
+
+
+  public T setDescription(String description) {
+    this.description = description;
+    return (T)this.returnBuilder();
+  }
+
+  public T setSubject(String subject) {
+    this.subject = subject;
+    return (T)this.returnBuilder();
+  }
+
+  public T setLocation(Location location) {
+    this.location = location;
+    return (T)this.returnBuilder();
+  }
+
+  public T setStatus(Status status) {
+    this.status = status;
+    return (T)this.returnBuilder();
+  }
+
+  public abstract EventInterface build();
+
+  protected abstract T returnBuilder();
 }
