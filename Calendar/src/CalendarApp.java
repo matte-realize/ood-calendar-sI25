611eb621569f65ccc09ae2e8ea4d102a9e941d26
paulwisnaskas <paul.wisnaskas@gmail.com>
2025-06-04 10:02:29 -0400

    Added beginning of controller

--- Calendar/src/CalendarApp.java
+++ Calendar/src/CalendarApp.java
@@ -1,5 +1,17 @@
-public class CalendarApp {
-  public static void main() {
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
 
+import Controller.CalendarController;
+import Model.CalendarModel;
+import View.CalendarView;
+
+public class CalendarApp {
+  public static void main(String[] args) {
+    CalendarModel model = new CalendarModel();
+    CalendarView view = new CalendarView();
+    CalendarController calendarController = new CalendarController(model, view);
+    calendarController.go(args);
   }
 }
--- Calendar/src/Controller/CalendarController.java
+++ Calendar/src/Controller/CalendarController.java
@@ -1,4 +1,53 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package Controller;
 
+import Model.CalendarModel;
+import View.CalendarView;
+import java.util.Scanner;
+
 public class CalendarController {
+  private CalendarModel calendarModel;
+  private CalendarView calendarView;
+
+  public CalendarController(CalendarModel calendarModel, CalendarView calendarView) {
+    this.calendarModel = calendarModel;
+    this.calendarView = calendarView;
+  }
+
+  public void go(String[] args) {
+    switch (args[1].toLowerCase()) {
+      case "interactive":
+        this.runInteractiveMode();
+        break;
+      case "headless":
+        this.runHeadlessMode(args[2]);
+    }
+
+  }
+
+  public void runInteractiveMode() throws IllegalArgumentException {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Type commands to view/edit calendar (type 'exit' to quit):");
+
+    String command;
+    do {
+      System.out.print("> ");
+      command = scanner.nextLine().trim();
+    } while(!this.processCommand(command));
+
+  }
+
+  public void runHeadlessMode(String filename) throws IllegalArgumentException {
+  }
+
+  private boolean processCommand(String command) throws IllegalArgumentException {
+    if (command.equals("exit")) {
+      return true;
+    }
+    return false;
+  }
 }
